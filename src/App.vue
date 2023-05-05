8f460f324528fb3dab396eeeb2f4e44d6ccadc2b
WaqasMehmoodCS <waqasmehmoodcs@gmail.com>
2023-05-05 18:29:14 +0500

    :sparkle: initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Section1 from "./components/Section1.vue"
+
+</script>
+
+<template>
+<div class="container">
+  <Section1 />
+</div>
+</template>
+
+<style scoped>
+.container{
+  background-color: #F6F1F1;
+  height: 50vh;
+  width: 70vw;
+  margin: auto;
+  margin-top: 25vh;
+  border-radius: 15px;
+  padding: 10px;
+  
+}
+</style>
---
+++ src/components/Section1.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {reactive, ref} from "vue"
+
+const task=ref('')
+const todos=reactive([])
+const addTodo =(task)=>{
+    todos.push(task)
+// console.log(todos)       
+    }
+    const del = (t,i)=>{
+        todos.splice(i,1)
+console.log(t)
+console.log(i)
+    }
+</script>
+<template>
+<div>
+<h1>TODO LIST</h1>
+</div>
+<div>
+    <label for="task">Task : </label>
+    <input id="task" placeholder="Enter Task" v-model="task" />
+</div>
+<button @click="addTodo(task)">Add Todo</button>
+<div>
+    <h1>Task List</h1>
+   <div v-for="t,i in todos">
+    
+    <input type="text" :placeholder="t"  />
+    <button @click="del(t,i)">Delete</button>
+
+   </div>
+</div>
+</template>
+<style scoped>
+
+
+</style>
